ddbd15f9881013fff9e1225019446cd880a76db2
Dhia Djobbi <dhia.djobbi@avaxia-group.com>
2022-07-10 16:36:43 +0100

    added generateURL Validation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Express, Request, Response } from "express";
 import { generateURL, redirectURL } from "../controllers/shortURL.controller";
+import { generateURLValidation } from "../middlewares/validators/generateURL.validation";
 
 const routes = (app: Express) => {
-  app.post("/api/url", generateURL);
+  app.post("/api/url", generateURLValidation, generateURL);
   app.get("/:shortID", redirectURL);
 };
 
