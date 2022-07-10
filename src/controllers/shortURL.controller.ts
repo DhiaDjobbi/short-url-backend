2ac6e6ba1cc3c1054c8da57e82204026f8ca51ce
Dhia Djobbi <dhia.djobbi@avaxia-group.com>
2022-07-10 14:50:33 +0100

    Created redirectURL Controller

--- src/controllers/shortURL.controller.ts
+++ src/controllers/shortURL.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import generateURLService from "../services/shortURL/generateURL.service";
+import findURLService from "../services/shortURL/findURL.service";
 
 export const generateURL = async (req: Request, res: Response) => {
   const { destination } = req.body;
@@ -10,3 +11,14 @@ export const generateURL = async (req: Request, res: Response) => {
   }
   return res.status(statusCode).json(createResult);
 };
+
+export const redirectURL = async (req: Request, res: Response) => {
+  const { shortID } = req.params;
+  const findResult = await findURLService(shortID);
+  if (findResult?.status === "error") {
+    return res.status(400).json(findResult);
+  }
+  if (findResult?.data) {
+    return res.redirect(findResult.data.destination);
+  }
+}
