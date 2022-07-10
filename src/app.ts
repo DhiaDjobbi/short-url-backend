1e2919cae65b89a7b93f8def5896132639e8874a
Dhia Djobbi <dhia.djobbi@avaxia-group.com>
2022-07-10 13:08:59 +0100

    Added BodyParser Middleware + Database Config

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
-import express from 'express';
-import config from 'config';
-import routes from './routes';
+import express from "express";
+import config from "config";
+import routes from "./routes";
+import bodyParser from "body-parser";
+import db from "./db";
 
 const app = express();
-
-const port = config.get('port');
+app.use(bodyParser.json());
+const port = config.get("port");
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    routes(app);
+  console.log(`Server is running on port ${port}`);
+  db();
+  routes(app);
 });
