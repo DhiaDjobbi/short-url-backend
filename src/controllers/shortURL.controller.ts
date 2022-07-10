03e0f15ecef6bc10108c85d5c58c7170d363c450
Dhia Djobbi <dhia.djobbi@avaxia-group.com>
2022-07-10 23:32:39 +0100

    Saving stats while redirecting

--- src/controllers/shortURL.controller.ts
+++ src/controllers/shortURL.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import generateURLService from "../services/shortURL/generateURL.service";
 import findURLService from "../services/shortURL/findURL.service";
+import createStatService from "../services/statistics/createStat.service";
+import findByUrlAndIPService from "../services/statistics/findByUrlAndIP.service";
 
 export const generateURL = async (req: Request, res: Response) => {
   const { destination } = req.body;
@@ -13,12 +15,27 @@ export const generateURL = async (req: Request, res: Response) => {
 };
 
 export const redirectURL = async (req: Request, res: Response) => {
+  // Find the short URL in DB
   const { shortID } = req.params;
-  const findResult = await findURLService(shortID);
-  if (findResult?.status === "error") {
-    return res.status(400).json(findResult);
+  const findUrlResult = await findURLService(shortID);
+  if (findUrlResult?.status === "error") {
+    return res.status(400).json(findUrlResult);
   }
-  if (findResult?.data) {
-    return res.redirect(findResult.data.destination);
+  if (findUrlResult?.data) {
+    // Get the IP address of the user
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    if (ip) {
+      const urlID = findUrlResult.data._id;
+      // Check if the user has already visited the URL
+      const findByUrlAndIPResult = await findByUrlAndIPService(urlID, ip.toString() );
+      if (findByUrlAndIPResult?.message === "Not found") {
+        // If the user has not visited the URL, create a new statistic
+        const createStatResult = await createStatService(urlID, ip.toString());
+        if (createStatResult?.status === "error") {
+          return res.status(400).json(createStatResult);
+        }
+      }
+    }
+    return res.redirect(findUrlResult.data.destination);
   }
-}
+};
